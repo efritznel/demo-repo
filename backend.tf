1254fb94a643009d114ec6ee9344420bbfc06610
Fritznel Etienne <efritznel@gmail.com>
2025-02-07 12:07:45 -0500

    new run2

--- backend.tf
+++ backend.tf
@@ -5,3 +5,12 @@ terraform {
     region = "us-east-1"
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-1"
+}
