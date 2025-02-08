456b78cde75fced36bae75d21dd294431c3f2050
Fritznel Etienne <efritznel@gmail.com>
2025-02-08 11:53:53 -0500

    github-action new env

--- asg.tf
+++ asg.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "my_template" {
   name_prefix   = "my-template"
   image_id      = "ami-085ad6ae776d8f09c"  # Replace with latest Amazon Linux AMI
   instance_type = "t2.micro"
-  key_name      = "demo-key"  # Replace with your key pair name
+  key_name      = "demokeypair"  # Replace with your key pair name
 
   network_interfaces {
     associate_public_ip_address = true
