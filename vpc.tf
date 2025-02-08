72cd82ac4b9981bbec87b8b03883b7ac53737825
Fritznel Etienne <efritznel@gmail.com>
2025-02-08 11:41:52 -0500

    github-action new env

--- vpc.tf
+++ vpc.tf
@@ -49,35 +49,4 @@ resource "aws_route_table_association" "my_rta" {
   route_table_id = aws_route_table.my_route_table.id
 }
 
-# Create Security Group
-resource "aws_security_group" "my_sg" {
-  vpc_id = aws_vpc.my_vpc.id
 
-  # Allow HTTP traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow SSH traffic
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "MySecurityGroup"
-  }
-}
